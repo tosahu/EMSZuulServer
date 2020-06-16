2b11fcaa750a5bcd45028ecbb5ef0052e6f03e98
SANJAY <SANJAY@HP>
2020-06-16 13:58:21 +0530

    First Commit

---
+++ EMSZuulServer/src/main/java/sks/spring/boot/app/zuulserver/EmsZuulServerApplication.java
@@ -0,0 +1,19 @@
+package sks.spring.boot.app.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class EmsZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsZuulServerApplication.class, args);
+	}
+
+}
---
+++ EMSZuulServer/src/test/java/sks/spring/boot/app/zuulserver/EmsZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sks.spring.boot.app.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
